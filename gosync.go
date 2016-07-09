0f702f68462e5f9ea587e640d77cc7b63171a883
Denmaseno <agus@senomas.com>
2016-07-09 12:29:09 +0700

    dev

--- gosync.go
+++ gosync.go
@@ -60,7 +60,7 @@ func main() {
 		}
 		for _, v := range files {
 			pLen += v.size
-			fmt.Printf("CHECK %v  %v", pLen, maxLen)
+			fmt.Printf("CHECK %v  %v\n", pLen, maxLen)
 			if maxLen == -1 || pLen < maxLen {
 				fmt.Printf("FILE [%s]  %s  %v\n", v.name, v.time.Format(time.RFC3339), v.size)
 			}
