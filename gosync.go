e6583cae004a4b5fa0cef9b6b4a28e741b190cf1
Denmaseno <agus@senomas.com>
2016-07-09 12:09:55 +0700

    dev

--- gosync.go
+++ gosync.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path/filepath"
 	"sort"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -30,26 +31,39 @@ func (files fileDatas) Swap(i, j int) {
 }
 
 func main() {
-	var files fileDatas
-	fp, _ := filepath.Abs(".")
-	fmt.Printf("PATH [%s]\n", fp)
-	filepath.Walk(fp, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		} else if info.IsDir() {
-			if strings.HasPrefix(info.Name(), ".") {
-				return filepath.SkipDir
+	if len(os.Args) > 1 && os.Args[1] == "list" {
+		var files fileDatas
+		fp, _ := filepath.Abs(".")
+		fmt.Printf("PATH [%s]\n", fp)
+		filepath.Walk(fp, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			} else if info.IsDir() {
+				if strings.HasPrefix(info.Name(), ".") {
+					return filepath.SkipDir
+				}
+			} else {
+				if !strings.HasPrefix(info.Name(), ".") {
+					fname := fmt.Sprintf("%s%c%s", path, os.PathSeparator, info.Name())
+					files = append(files, fileData{name: fname, time: info.ModTime(), size: info.Size()})
+				}
 			}
+			return err
+		})
+		sort.Sort(files)
+		var maxLen, pLen int64
+		if len(os.Args) > 2 {
+			maxLen = strconv.ParseInt(os.Args[2], 10, 64) * 1024
 		} else {
-			if !strings.HasPrefix(info.Name(), ".") {
-				fname := fmt.Sprintf("%s%c%s", path, os.PathSeparator, info.Name())
-				files = append(files, fileData{name: fname, time: info.ModTime(), size: info.Size()})
+			maxLen = -1
+		}
+		for _, v := range files {
+			pLen += v.size
+			if maxLen == -1 && pLen < maxLen {
+				fmt.Printf("FILE [%s]  %s  %v\n", v.name, v.time.Format(time.RFC3339), v.size)
 			}
 		}
-		return err
-	})
-	sort.Sort(files)
-	for _, v := range files {
-		fmt.Printf("FILE [%s]  %s  %v\n", v.name, v.time.Format(time.RFC3339), v.size)
+	} else {
+		fmt.Printf("FORMAT\n  gosync list\n  gosync hash\n")
 	}
 }
